abd1a9fd240ee45cb99a01be60fc543930c94f83
Somesh Sharma <85858871+SRSKing12@users.noreply.github.com>
2022-07-30 21:31:18 +0530

    Update urls.py

--- shoppinglyx/urls.py
+++ shoppinglyx/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django_email_verification import urls as email_urls
+from shoppinglyx.settings import ADMIN_URL
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(ADMIN_URL, admin.site.urls),
     path('', include('app.urls')),
     path('email/', include(email_urls)),
 ]
