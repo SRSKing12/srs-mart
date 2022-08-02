ec778ad0dbdaf9d5fb86b0eb255848af634aaa37
Somesh Sharma <85858871+SRSKing12@users.noreply.github.com>
2022-08-02 18:51:43 +0530

    Update urls.py

--- shoppinglyx/urls.py
+++ shoppinglyx/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django_email_verification import urls as email_urls
 from shoppinglyx.settings import ADMIN_URL
+from django.urls.conf import re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path(ADMIN_URL, admin.site.urls),
     path('', include('app.urls')),
     path('email/', include(email_urls)),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
